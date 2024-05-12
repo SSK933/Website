333cdcce7e36408f127e7c64cf0307556cd1e2e9
SSK933 <158428273+SSK933@users.noreply.github.com>
2024-05-11 23:55:56 -0500

    StatsSection implemented.

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ var tl = gsap.timeline({
     scrollTrigger:{
         trigger: ".page1 h1",
         scroller: ".main",
-        //markers: true,
-        start: "top 44%",
+        markers: true,
+        start: "top 42%",
         end:"top 18%",
         scrub:4
     }
@@ -146,3 +146,48 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 100); // Delay in milliseconds (300ms in this example)
     });
 });
+
+
+const workSection = document.querySelector(".section-work-data");
+const workObserver = new IntersectionObserver(
+  (entries, observer) => {
+    const [entry] = entries;
+    // console.log(entry);
+
+    // if (entry.isIntersecting == false)
+    if (!entry.isIntersecting) return;
+
+    // animate number counter
+
+    const counterNum = document.querySelectorAll(".counter-numbers");
+
+    const speed = 200;
+
+    counterNum.forEach((curElem) => {
+      const updateNumber = () => {
+        const targetNumber = parseInt(curElem.dataset.number);
+        // console.log(targetNumber);
+        const initialNum = parseInt(curElem.innerText);
+        // console.log(initialNum);
+
+        const incrementNumber = Math.trunc(targetNumber / speed);
+        // console.log(incrementNumber);
+
+        if (initialNum < targetNumber) {
+          curElem.innerText = `${initialNum + incrementNumber}+`;
+          setTimeout(updateNumber, 10);
+        }
+      };
+
+      updateNumber();
+    });
+
+    observer.unobserve(workSection);
+  },
+  {
+    root: null,
+    threshold: 0,
+  }
+);
+
+workObserver.observe(workSection);
